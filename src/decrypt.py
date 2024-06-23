f341cfa5e1c8de9e47b537a4222e1a8e69f3050a
bradinator <123034682+Ulladay-Hub@users.noreply.github.com>
2024-06-23 12:39:23 +1000

    Create decrypt.py

---
+++ src/decrypt.py
@@ -0,0 +1,16 @@
+from cryptography.fernet import Fernet
+from utils import log
+
+# Not working yet. Still making logic
+# SECRET_KEY = b'secret-key'
+
+def decrypt_package(package_data):
+    log("INFO", "Decrypting package")
+    cipher_suite = Fernet(SECRET_KEY)
+    try:
+        decrypted_data = cipher_suite.decrypt(package_data)
+        log("INFO", "Package decrypted successfully")
+        return decrypted_data
+    except Exception as e:
+        log("ERROR", f"Failed to decrypt package: {e}")
+        return None
